6e3a69f726b0c5ced417c0a47f801100eb480cbb
Ana sofia Gonzalez <soft.ann14@gmail.com>
2025-02-21 18:03:03 -0500

    AAAA

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const notasEstudiantes = [100,50,65,70,85,96,45];
+function clasificar (notasEstudiantes){
+    const notasLetras = [A,B,C,D,E,F] 
+    array.forEach(element => {
+        
+    })};
